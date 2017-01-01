16ec845af3310024c3af0a6440627552bc16b285
Nikita Korolkov <n.korolkov@rambler-co.ru>
2017-01-02 01:12:40 +0300

    edit supervisor file;

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 
-echo 'ACTIVATED django_telegram';
-alias django_telegram_managepy="/home/ubuntu/.virtualenvs/django_telegram/bin/python /home/ubuntu/parsing_news_and_send_telegram/manage.py";
+echo 'ACTIVATED django_telegram'
+alias django_telegram_managepy="/home/ubuntu/.virtualenvs/django_telegram/bin/python /home/ubuntu/parsing_news_and_send_telegram/manage.py"
 
 
 django_telegram_managepy parsing_cinema_sites &
 django_telegram_managepy parsing_news_sites &
 django_telegram_managepy reply_on_telegram_messages &
 
-django_telegram_managepy runserver 0.0.0.0:8000 &
+django_telegram_managepy runserver 0.0.0.0:8000
